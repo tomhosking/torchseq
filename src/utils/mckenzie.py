d4e77a4ef2803f1d19d04792dad6c70d43c40baf
Tom <tomhosking@gmail.com>
2020-03-26 16:31:06 +0000

    Update mckenzie

--- src/utils/mckenzie.py
+++ src/utils/mckenzie.py
@@ -4,11 +4,14 @@ def update_mckenzie(progress, metric):
 
     try:
         job_id = os.environ['SLURM_JOB_ID']
+        partition = os.environ['SLURM_JOB_PARTITION']
         endpoint = os.environ['MCKENZIE_ENDPOINT']
+        
         requests.post(
             'http://' + endpoint+'/hooks/update_job/',
             data={
                 'jobid': job_id,
+                'partition': partition,
                 'progress': progress,
                 'metric': metric
             }
