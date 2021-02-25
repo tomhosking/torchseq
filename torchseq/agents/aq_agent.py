Adjust loss calculation for mbart, fixes

torchseq/agents/aq_agent.py
@@ -75,16 +75,16 @@ class AQAgent(ModelAgent):
 
         if self.config.training.get("xe_loss", True):
             xe_loss = self.loss(logits.permute(0, 2, 1), batch[tgt_field])
-            # This isn't necessary - the teacher force decoder already forces the first token
-            # if self.config.training.get("loss_offset", 0) > 0:
-            #     xe_offset = self.config.training.get("loss_offset", 0)
-            #     # xe_mask = torch.ones_like(xe_loss)
-            #     # xe_mask[:, :xe_offset] = 0
-            #     xe_loss = xe_loss[:, xe_offset:]
-            #     # print(torch.argmax(logits, dim=-1)[0])
-            #     # print(batch[tgt_field][0].shape)
-            #     # print(xe_loss[0].shape)
-            #     # exit()
+            # Although the teacher force decoder already forces the first token, it's nice not to get crazy losses recorded
+            if self.config.training.get("loss_offset", 0) > 0:
+                xe_offset = self.config.training.get("loss_offset", 0)
+                # xe_mask = torch.ones_like(xe_loss)
+                # xe_mask[:, :xe_offset] = 0
+                xe_loss = xe_loss[:, xe_offset:]
+                # print(torch.argmax(logits, dim=-1)[0])
+                # print(batch[tgt_field][0].shape)
+                # print(xe_loss[0].shape)
+                # exit()
             this_loss += xe_loss.sum(dim=1) / (batch[tgt_field + "_len"] - 1).to(this_loss)
 
         if self.config.training.suppression_loss_weight > 0:

torchseq/models/bottleneck_autoencoder.py
@@ -193,8 +193,8 @@ class BottleneckAutoencoderModel(nn.Module):
             memory["encoding"] = encoding_pooled
             memory["encoding_mask"] = None
 
-            memory["sep_encoding_1"] = encoding_pooled[:, :, :sep_splice_ix].detach()
-            memory["sep_encoding_2"] = encoding_pooled[:, :, sep_splice_ix:].detach()
+            memory["sep_encoding_1"] = encoding_pooled[:, :, :sep_splice_ix].max(dim=1, keepdim=True).values.detach()
+            memory["sep_encoding_2"] = encoding_pooled[:, :, sep_splice_ix:].max(dim=1, keepdim=True).values.detach()
 
         # Fwd pass through decoder block
         logits, memory = self.seq_decoder(output, memory)

torchseq/models/samplers/teacher_force.py
@@ -24,7 +24,7 @@ class TeacherForcedSampler(nn.Module):
         )
 
         if MBART_HACK:
-            # logits[:, :, Tokenizer().bos_id] = float("-1e18")
+            # TODO: Use the actual lang id here!!
             logits[:, :, 250004] = float("1e18")
         else:
             logits[:, :, Tokenizer().bos_id] = float("1e18")