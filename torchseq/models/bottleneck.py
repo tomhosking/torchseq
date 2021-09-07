Head masking, fix Rouge, bug fixes

torchseq/models/bottleneck.py
@@ -87,7 +87,7 @@ class PoolingBottleneck(nn.Module):
                 **quantizer_kwargs,
             )
 
-    def forward(self, encoding, memory, global_step, forced_codes=None):
+    def forward(self, encoding, memory, global_step, forced_codes=None, head_mask=None):
 
         # Pool
         encoding_pooled = (
@@ -113,7 +113,7 @@ class PoolingBottleneck(nn.Module):
             #     raise Exception("Arbitrary quantizer residual ranges are not currently supported")
 
             vq_loss, quantized_encoding, quantizer_indices = self.quantizer(
-                encoding_pooled[:, :, splice_ix:], global_step, forced_codes
+                encoding_pooled[:, :, splice_ix:], global_step, forced_codes, head_mask
             )
 
             encoding_pooled = torch.cat([encoding_pooled[:, :, :splice_ix], quantized_encoding], dim=-1)

torchseq/models/bottleneck_autoencoder.py
@@ -54,7 +54,9 @@ class BottleneckAutoencoderModel(nn.Module):
                 include_position=self.config.encoder.get("position_embeddings", True),
             )
 
-            encoding_pooled, memory = self.bottleneck(encoding, memory, batch["_global_step"])
+            encoding_pooled, memory = self.bottleneck(
+                encoding, memory, batch["_global_step"], head_mask=batch.get("head_mask", None)
+            )
 
             raw_encoding_pooled = memory["encoding_pooled"]
 
@@ -67,7 +69,11 @@ class BottleneckAutoencoderModel(nn.Module):
             if self.config.bottleneck.get("split_encoder", False):
                 encoding2, memory = self.seq_encoder(batch[self.src_field], batch[self.src_field + "_len"], memory)
                 encoding_pooled2, memory = self.bottleneck(
-                    encoding2, memory, batch["_global_step"], forced_codes=batch.get("forced_codes", None)
+                    encoding2,
+                    memory,
+                    batch["_global_step"],
+                    forced_codes=batch.get("forced_codes", None),
+                    head_mask=batch.get("head_mask", None),
                 )
 
                 # TODO: Instead of 2x full size encoders + down projection, change to 2x half size encoders
@@ -101,6 +107,7 @@ class BottleneckAutoencoderModel(nn.Module):
                     template_memory,
                     batch["_global_step"],
                     forced_codes=batch.get("forced_codes", None),
+                    head_mask=batch.get("head_mask", None),
                 )
 
                 if "loss" in memory:

torchseq/models/vq_code_predictor.py
@@ -132,6 +132,8 @@ class VQCodePredictor(torch.nn.Module):
             if self.config.get("autoregressive_lstm", False)
             else None
         )
+
+        beam_width = self.config.get("beam_width", 3)
         # print(encoding.shape, seq_init.shape)
         outputs = self.classifier(encoding, seq=seq_init)
 
@@ -162,8 +164,8 @@ class VQCodePredictor(torch.nn.Module):
                         curr_logits = self.classifier(encoding[bix].unsqueeze(0), seq)[0, h_ix, :]
                     else:
                         curr_logits = logits[h_ix, :]
-                    probs, predicted = torch.topk(torch.softmax(curr_logits, -1), 3, -1)
-                    for k in range(2):
+                    probs, predicted = torch.topk(torch.softmax(curr_logits, -1), beam_width, -1)
+                    for k in range(beam_width):
                         new_hyp = [copy.copy(combo), copy.copy(prob)]
                         new_hyp[0].append(predicted[k].item())
                         new_hyp[1] += torch.log(probs[k]).item()
@@ -171,7 +173,7 @@ class VQCodePredictor(torch.nn.Module):
                         new_hypotheses.append(new_hyp)
 
                 joint_probs = new_hypotheses
-                joint_probs = sorted(joint_probs, key=lambda x: x[1], reverse=True)[:3]
+                joint_probs = sorted(joint_probs, key=lambda x: x[1], reverse=True)[:beam_width]
             pred_codes = [x[0] for x in sorted(joint_probs, key=lambda x: x[1], reverse=True)[:2]]
             all_pred_codes.append(pred_codes)
 

torchseq/models/vq_vae.py
@@ -100,7 +100,10 @@ class VectorQuantizerMultiHead(nn.Module):
         self._norm_loss_weight = norm_loss_weight
         self._full_dim_input = full_dim_input
 
-        self._head_dropout = head_dropout
+        if head_dropout is not None and head_dropout > 0:
+            self._head_dropout = torch.distributions.Bernoulli(1 - head_dropout)
+        else:
+            self._head_dropout = None
 
         if hierarchical:
 
@@ -193,7 +196,7 @@ class VectorQuantizerMultiHead(nn.Module):
     def encoding_to_logits(self, input, head_ix, prev_codes):
         pass
 
-    def forward(self, inputs, global_step=None, forced_codes=None):
+    def forward(self, inputs, global_step=None, forced_codes=None, head_mask=None):
         input_shape = inputs.shape
 
         quantized_list = []
@@ -334,7 +337,7 @@ class VectorQuantizerMultiHead(nn.Module):
                 min_k = torch.topk(all_probs[head_ix], this_offset + 1, dim=1, largest=False).indices
                 vq_codes.append(min_k[:, this_offset])
         else:
-            vq_codes = [torch.argmax(probs, dim=-1) for probs in all_probs]
+            vq_codes = [torch.argmax(probs, dim=-1).squeeze() for probs in all_probs]
 
         # Now that we have the codes, calculate their embeddings
         out_embeds = self._output_embedding if self._output_embedding is not None else self._embedding
@@ -359,10 +362,20 @@ class VectorQuantizerMultiHead(nn.Module):
         else:
             quantized = torch.cat(quantized_list, dim=1)
 
+            if head_mask is not None:
+                assert (
+                    head_mask.shape[1] == self._num_heads
+                ), "If head_mask is set, it must be the same length as the number of quantizer heads! {:} vs {:}".format(
+                    head_mask.shape[1], self._num_heads
+                )
+                # print(vq_codes[0].shape)
+                # print(quantized_list[0].shape)
+                # print(head_mask.shape, quantized.shape)
+                quantized = quantized * head_mask.unsqueeze(-1)
+
         if self._additive:
-            if self._head_dropout is not None and self._head_dropout > 0:
-                dist = torch.distributions.Bernoulli(1 - self._head_dropout)
-                mask = dist.sample(sample_shape=(*quantized.shape[:-1], 1))
+            if self._head_dropout is not None and self.training:
+                mask = self._head_dropout.sample(sample_shape=(*quantized.shape[:-1], 1))
                 mask = torch.cumprod(mask, dim=1).to(quantized.device)
                 quantized = quantized * mask
             quantized = torch.sum(quantized, dim=1)