290840b112d515c5f402e40887134a0ab3308dcd
Tom <tomhosking@gmail.com>
2023-10-23 14:05:51 +0100

    Add pbar to pretrained NLI model

--- torchseq/pretrained/nli.py
+++ torchseq/pretrained/nli.py
@@ -1,6 +1,18 @@
 from typing import List
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 import torch
+from tqdm import tqdm
+
+
+class ListDataset(torch.utils.data.Dataset):
+    def __init__(self, original_list):
+        self.original_list = original_list
+
+    def __len__(self):
+        return len(self.original_list)
+
+    def __getitem__(self, i):
+        return self.original_list[i]
 
 
 class PretrainedNliModel:
@@ -24,12 +36,24 @@ class PretrainedNliModel:
         self.pipe = pipeline("text-classification", model=self.model, tokenizer=self.tokenizer, device=0)
 
     def get_scores(
-        self, premises: List[str], hypotheses: List[str], return_entailment_prob: bool = True, bsz: int = 64
+        self,
+        premises: List[str],
+        hypotheses: List[str],
+        return_entailment_prob: bool = True,
+        bsz: int = 64,
+        progress: bool = False,
     ):
-        dataset = [{"text": p, "text_pair": h} for p, h in zip(premises, hypotheses)]
+        dataset = ListDataset([{"text": p, "text_pair": h} for p, h in zip(premises, hypotheses)])
 
-        outputs = self.pipe(dataset, batch_size=bsz, top_k=None, num_workers=2)
-        outputs = [{x["label"]: x["score"] for x in res} for res in outputs]
+        outputs = [
+            {x["label"]: x["score"] for x in res}
+            for res in tqdm(
+                self.pipe(dataset, batch_size=bsz, top_k=None, num_workers=2),
+                disable=(not progress),
+                total=len(dataset),
+            )
+        ]
+        # outputs = [{x["label"]: x["score"] for x in res} for res in outputs]
 
         if return_entailment_prob:
             return [p["ENTAILMENT"] for p in outputs]
